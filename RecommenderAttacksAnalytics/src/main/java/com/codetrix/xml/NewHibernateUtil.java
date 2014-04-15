b4858967ce23df1d8315bb3ccc5f131a832d977f
mikhail123 <msutormin@gmail.com>
2014-04-15 20:32:26 +1000

    NewHibernateUtil
    I suppose it changes Hibernate parameters on the fly without actually
    writing them into hibernate.cfg.xml
    
    Changing the actual xml is beyond my knowledge of Java even with all
    those libraries available online.

---
+++ RecommenderAttacksAnalytics/src/main/java/com/codetrix/xml/NewHibernateUtil.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.codetrix.xml;
+
+/**
+ *
+ * @author Mikhail
+ */
+
+public class NewHibernateUtil {
+
+private final SessionFactory sessionFactory;
+ 
+ String host;
+ String database;
+ String username;
+ String password;
+ 
+ NewHibernateUtil (String host,String database,String username,String password){
+     this.host=host;
+     this.database=database;
+     this.username=username;
+     this.password=password;
+     AnnotationConfiguration ac = new AnnotationConfiguration().configure();
+     ac.setProperty("hibernate.connection.url", "jdbc:mysql://"+host+"/"+database+"");  
+     ac.setProperty("hibernate.connection.username", username);  
+     ac.setProperty("hibernate.connection.password", password);             
+     sessionFactory = ac.buildSessionFactory();         
+ }    
+
+public SessionFactory getSessionFactory() {
+    return sessionFactory;
+}    
+}
